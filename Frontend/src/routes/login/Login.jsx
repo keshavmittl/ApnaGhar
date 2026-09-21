f72150de0ac8b697158ee86cd372d0e581d92f87
Keshav Mittal <146485463+keshavmittl@users.noreply.github.com>
2026-09-22 02:15:33 +0530

    fix case sensitive import in Login.jsx

--- Frontend/src/routes/login/Login.jsx
+++ Frontend/src/routes/login/Login.jsx
@@ -2,7 +2,7 @@ import { useState, useContext } from "react";
 import "./login.scss";
 import { Link, useNavigate } from "react-router-dom";
 import apiRequest from "../../lib/apiRequest";
-import { AuthContext } from "../../context/AuthContext";
+import { AuthContext } from "../../context/authContext";
 import Logo from "../../components/logo/Logo";
 
 function Login() {
